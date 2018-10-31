fec3af1215e84b8183b622503188bd3c29c119a8
ilyamore88 <themorozik@gmail.com>
2018-10-31 19:44:48 +0300

    init project files

---
+++ views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import json
+
+
+def indexRender(request):
+    return render(request, 'index.html', {})
+
+
+def cinemaRender(request):
+    return render(request, 'pages/cinema.html', {})
+
+
+def movieRender(request):
+    return render(request, 'pages/movie.html', {})
+
+
+def personRender(request):
+    return render(request, 'pages/person.html', {})
+
+
+def signinRender(request):
+    return render(request, 'pages/signin.html', {})
+
+
+def signupRender(request):
+    return render(request, 'pages/signup.html', {})
